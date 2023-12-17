f6e3c9407364b5a9f514e785b96349827d991cbe
A8250092 <stanley.mei08@gmail.com>
2023-12-17 18:53:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,3 +34,21 @@ a=3
 print(a+2.0)
 a=a+1.0
 print(a)
+
+print(2<3)
+print(2.0>3.0)
+print(2<=2)
+print(2<2)
+print(2==2)
+
+
+print(4.0 > 3.999)
+print(4 > 4)
+print(4 > + 4)
+
+print(3+5.0)
+print(5/2)
+print(5/2==5/2.0)
+print(round(2.6))
+print(int(2.6))
+print(5*2==5.0*2.0)
